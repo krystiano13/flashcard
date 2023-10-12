4504ba4bdb93511afa560f515c0a6395f96286f4
krystiano13 <crystianotv@gmail.com>
2023-10-12 11:28:25 +0200

    create form appear animation

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
-<?php
-    session_start();
-    require_once 'vendor/autoload.php';
-?>
+<?php session_start() ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,13 +8,13 @@
     <meta name="keywords" content="Flashcard, app, learn, study" >
     <meta name="author" content="Krystian Zieja">
     <meta name="description" content="Simple flashcard app">
-    <link rel="stylesheet" href="./styles/micro-css-framework/index.css" type="text/css" />
-    <link rel="stylesheet" href="./styles/form.css" type="text/css" />
+    <link rel="stylesheet" href="./styles/micro-css-framework/index.css" />
+    <link rel="stylesheet" href="./styles/login.css?v=11" />
     <script defer src="dist/login.js"></script>
 </head>
 <body>
     <main class="width-100 height-100 d-flex jc-center ai-center bg">
-        <form method="post" class="d-flex flex-col ai-center jc-center bg-primary p-4 pt-6 pb-6 br-rad-1">
+        <form id="form" method="post" class="d-flex flex-col ai-center jc-center bg-primary p-4 pt-6 pb-6 br-rad-1">
             <input id="usernameInput" class="m-1 mt-3 mb-3 font-other p-1 outline-none br-none br-b-solid br-b-2 br-b-accent bg-secondary color f-s" type="text" name="username" placeholder="username">
             <input id="passwordInput" class="m-1 mt-3 mb-3 font-other p-1 outline-none br-none br-b-solid br-b-2 br-b-accent bg-secondary color f-s" type="password" name="password" placeholder="password" />
             <button class="br-none bg-accent-hover color-bg-hover mt-3 mb-3
