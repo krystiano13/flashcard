ecf3c214461696d9a1e2169dfc17104222c6d327
krystiano13 <crystianotv@gmail.com>
2023-10-14 15:53:04 +0200

    add appear animations in deck view

--- deck.php
+++ deck.php
@@ -15,10 +15,10 @@
 </head>
 <body>
     <main class="width-100 height-100 d-flex jc-center ai-center">
-        <div class="el d-flex jc-center ai-center font-head m-2 bg-primary color-bg f-600 f-xl c-pointer">
+        <div id="start" class="el bg-hover bg-accent-hover d-flex br-rad-2 jc-center ai-center font-head m-2 bg-primary color-bg f-600 f-xl c-pointer">
             Start Learning
         </div>
-        <div class="el d-flex jc-center ai-center font-head m-2 bg-primary color-bg f-600 f-xl c-pointer">
+        <div id="add" class="el bg-hover bg-accent-hover d-flex br-rad-2 jc-center ai-center font-head m-2 bg-primary color-bg f-600 f-xl c-pointer">
             Add/Remove Cards
         </div>
     </main>
