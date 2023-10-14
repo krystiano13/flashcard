b55c685e3dd39f84dac3710ff1e2a95f3b18a169
krystiano13 <crystianotv@gmail.com>
2023-10-14 15:37:25 +0200

    create structure and basic styles for deck view

---
+++ deck.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Flashcards</title>
+    <meta name="keywords" content="Flashcard, app, learn, study">
+    <meta name="author" content="Krystian Zieja">
+    <meta name="description" content="Simple flashcard app">
+    <link rel="stylesheet" href="./styles/micro-css-framework/index.css"/>
+    <link rel="stylesheet" href="./styles/deck.css?v=11"/>
+</head>
+<body>
+    <main class="width-100 height-100 d-flex jc-center ai-center">
+        <div class="el d-flex jc-center ai-center font-head m-2 bg-primary color-bg f-600 f-xl c-pointer">
+            Start Learning
+        </div>
+        <div class="el d-flex jc-center ai-center font-head m-2 bg-primary color-bg f-600 f-xl c-pointer">
+            Add/Remove Cards
+        </div>
+    </main>
+</body>
+</html>
