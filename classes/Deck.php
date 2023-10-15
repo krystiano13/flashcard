3dfd96734187a8c1d86b128c46be0e6af5c5c63c
krystiano13 <crystianotv@gmail.com>
2023-10-15 17:09:17 +0200

    create class that is responsible for operations on decks

---
+++ classes/Deck.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\classes;
+require_once 'Database.php';
+
+use App\Database;
+
+class Deck
+{
+    private string $username;
+    private string $deckName;
+
+    public function __construct(string $username, string $name)
+    {
+        $this -> username = $username;
+        $this -> deckName = $name;
+    }
+
+    public function createDeck() {
+        $database = new Database();
+        $database -> connect();
+        $con = $database -> connection;
+
+        $query = $con -> prepare(
+            "INSERT INTO decks VALUES(NULL, :username, :name, 0)"
+        );
+
+        $query -> bindValue(':username', $this -> username);
+        $query -> bindValue(':name', $this -> deckName);
+
+        if($query -> execute()) {
+            echo json_encode(['status' => true]);
+        }
+        else {
+            echo json_encode(['status' => false]);
+        }
+    }
+}
