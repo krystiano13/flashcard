6f6a3e2b7ca1455f04e6c596d529eca045728c99
krystiano13 <crystianotv@gmail.com>
2023-10-18 12:32:32 +0200

    create intro animations for cards view

--- modules/cardList.php
+++ modules/cardList.php
@@ -4,7 +4,7 @@ function generateCardListItem(string $content, int $id):string {
     return <<<ELEMENT
     <div id="{$id}" 
     class="cardElement f-s d-flex br-rad-2 ai-center jc-between width-50 bg-secondary color mt-2 font-other f-400 m-1 p-2">
-        <label>{$content}</label>
+        <label class="max-width-50">{$content}</label>
         <section>
             <button class="br-none c-pointer font-head p-1 pl-3 pr-3 bg-primary color-bg">Edit</button>
             <button class="br-none c-pointer font-head p-1 pl-3 pr-3 bg-primary color-bg">Delete</button>
