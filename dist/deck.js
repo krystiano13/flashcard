56159b8cfb66e59168fb7faa26fa9e8d28b6bfc2
krystiano13 <crystianotv@gmail.com>
2023-10-18 12:50:06 +0200

    implement redirecting to cards view

--- dist/deck.js
+++ dist/deck.js
@@ -1,6 +1,11 @@
 "use strict";
 const backButton = document.querySelector('#back');
+const addButton = document.querySelector('#add');
 const goBack = () => {
     window.location.href = '/flashcard/panel.php';
 };
+const goCardsView = () => {
+    window.location.href = '/flashcard/cards.php';
+};
 backButton === null || backButton === void 0 ? void 0 : backButton.addEventListener('click', goBack);
+addButton === null || addButton === void 0 ? void 0 : addButton.addEventListener('click', goCardsView);
--- src/deck.ts
+++ src/deck.ts
@@ -1,7 +1,13 @@
 const backButton: HTMLButtonElement | null = document.querySelector('#back');
+const addButton: HTMLButtonElement | null = document.querySelector('#add');
 
 const goBack = () => {
     window.location.href = '/flashcard/panel.php';
 }
 
+const goCardsView = () => {
+    window.location.href = '/flashcard/cards.php';
+}
+
 backButton?.addEventListener('click', goBack);
+addButton?.addEventListener('click', goCardsView);
