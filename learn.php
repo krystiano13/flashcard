a5a1d791ecf006853fd2fa0cd1929cce3f1dd466
krystiano13 <crystianotv@gmail.com>
2023-10-22 12:00:38 +0200

    create basic styles

--- learn.php
+++ learn.php
@@ -8,15 +8,16 @@ if(!isset($_SESSION['deck'])) {
 <html lang="en">
 <head>
     <?php include "./components/head.php"; ?>
+    <link rel="stylesheet" href="./styles/learn.css" />
 </head>
 <body>
     <main class="width-100 height-100 d-flex ai-center jc-center">
-        <button id="left">Previous</button>
-        <div id="card">
-            <p>Tekst</p>
-            <label>click to swipe</label>
+        <button class="c-pointer width-10 color-bg bg-primary font-head br-none p-2 m-1" id="left">Previous</button>
+        <div class="c-pointer m-1 bg-primary color-bg width-25 height-75 d-flex flex-col jc-evenly ai-center" id="card">
+            <p class="font-head">Tekst</p>
+            <label class="font-other">click to swipe</label>
         </div>
-        <button id="right">Next</button>
+        <button class="c-pointer width-10 color-bg bg-primary font-head br-none p-2 m-1" id="right">Next</button>
     </main>
 </body>
 </html>
